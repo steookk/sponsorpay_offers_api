ecde5b5293d0ef8b313d50210e48a629369973ee
stefano <ustefano@gmail.com>
2014-04-04 18:12:27 +0200

    rails empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SponsorpayOfferApi::Application.config.secret_token = 'b99732bb1f8a3a311707da2fcc73f6d5f3eddc2abbb7f6a3b3f9a66c76c8e4150d8dcbfdceca9653d0a829e4719525976f636e0636c9bfbc24858d2e30a6e143'
